757b6bb60cdc8431b52c2ecc118be8d6be57848a
xummer <willx@gobeta.com.cn>
2014-12-29 17:09:51 +0800

    Initial Commit

---
+++ IBTWebViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IBTWebViewController
+//
+//  Created by Xummer on 14/12/29.
+//  Copyright (c) 2014年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IBTWebViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IBTWebViewController
+//
+//  Created by Xummer on 14/12/29.
+//  Copyright (c) 2014年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
